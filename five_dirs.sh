ccba43cda2f831d715984c7161e11e9c378995f3
Zixuan <Zixuan>
2022-10-21 12:24:55 -0500

    finished

--- five_dirs.sh
+++ five_dirs.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+mkdir five
+cd five
+for i in $(seq 1 5); do
+    mkdir dir$i
+    cd dir$i
+    for j in $(seq 1 4); do
+	for k in $(seq 1 $j); do
+	    echo $j >>file$j
+	done
+    done
+    cd ..
+done
